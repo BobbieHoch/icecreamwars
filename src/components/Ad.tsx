b75c67cc265556e06fa18e9c61d6af041c138239
BobbieHoch <rwhochstadt@gmail.com>
2023-01-24 23:22:05 -0500

    colorthemefontsizeflavorworking

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,19 +1,24 @@
 import "../CSS/Ad.css";
-
+//to display child component need interface
 interface AdProperties {
     flavor: string,
     isLight: boolean,
-    fontSize: number,
+    fontSize: number
 }
 
-export function Ad(props: AdProperties) {
+//props go from
+export function Ad(props:AdProperties) {
     let className = "Ad " + (props.isLight ? "Ad--light" : "Ad--dark");
     //Ad Ad--light
     //Ad Ad--light
     return (
         <div className={className}>
+                 
+        {/* <p>{props.isLight.toString()}</p>
+        <p>{props.fontSize}</p> */}
             <p>Vote For:</p>
-            <p style={{ fontSize: props.fontSize}}>{props.flavor}</p>
+            {/* <p>{props.flavor}</p> */}
+        <p style={{ fontSize: props.fontSize}}>{props.flavor}</p>
         </div>
     )
 }
--- src/components/AdDesign.tsx
+++ src/components/AdDesign.tsx
@@ -1,36 +1,45 @@
-import {useState}from "react"
+import { useState } from "react";
+import { Ad} from './Ad'
 
 export function AdDesign() {
     //state hook has Two Parts
     //Variable to track the state, --set the value
     //Function used to change the state
-    const [flavor, setFlavor] = useState('Chocolate');
+    const [flavor, setFlavor] = useState("Vanilla");
     const [isLight, setIsLight] = useState(true);
-
+    const [fontSize, setFontSize] = useState<number>(24);
 
     return (
         <div id="AdDesign">
             <h2>Ad Design</h2>
+
             <h2>What to Support:</h2>
+            
+            <Ad flavor = {flavor} isLight={isLight} fontSize={fontSize} />
+            {/* passes changing variable to parent- from the child Ad */}
 
             <div className="button-bar">
-                <button>Chocolate</button>
-                <button>Vanilla</button>
-                <button>Strawberry</button>
+                <button onClick={() => setFlavor("Chocolate")}>Chocolate</button>
+                <button onClick={() => setFlavor("Vanilla")}>Vanilla</button>
+                <button onClick={() => setFlavor("Strawberry")}>
+                    Strawberry
+                </button>
             </div>
 
             <h2>Color Theme</h2>
+
             <div className="button-bar">
-                <button>Light</button>
-                <button>Dark</button>
+                <button onClick={() => setIsLight(true)}>Light</button>
+                <button onClick={() => setIsLight(false)}>Dark</button>
             </div>
-            
-           {/* <div> <h2>Font Size</h2>
-           <div className="button-bar">
-            <button>Up</button>
-            <input type="number"> </input>
-            <button>Down</button> 
-            </div> */}
+
+            <h2>Font Size</h2>
+
+            <div className="button-bar">
+                <button onClick={() => setFontSize(fontSize + 1)}>Up</button>
+                <input type="number" value={fontSize}></input>
+                <button onClick={() => setFontSize(fontSize - 1)}>Down</button>
+            </div>
         </div>
-    )
+    );
 }
